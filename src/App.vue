996c0d31c13e0b14ba12322ec1b9a4da0567e71f
abel994 <abel994@hotmail.com>
2024-02-16 20:55:37 -0600

    57. Detectando el si el carrito esta vacio o tiene elementos

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import {ref, reactive, onMounted} from 'vue'
 import {db} from './data/guitarras'
 import Guitarra from './components/Guitarra.vue'
-import Header from './components/header.vue'
+import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 
 
@@ -22,7 +22,9 @@ const agregarCarrito = (guitarra) => {
 
 <template>
 
-    <Header />
+    <Header 
+        :carrito = "carrito"
+    />
 
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
