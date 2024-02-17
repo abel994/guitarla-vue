34931270a17327dba8261f509a3e8a2fdce3eb01
abel994 <abel994@hotmail.com>
2024-02-16 22:10:01 -0600

    58. Mostrando el Contenido del carrito

--- src/components/header.vue
+++ src/components/header.vue
@@ -41,13 +41,21 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
+                                        <tr
+                                            v-for="producto in carrito"
+                                        >
                                             <td>
-                                                <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
+                                                <img 
+                                                class="img-fluid"
+                                                :src="'/img/'+ producto.imagen + '.jpg'"
+                                                :alt="'imagen guitarra '+ producto.nombre"
+                                                >
                                             </td>
-                                            <td>SRV</td>
+                                            <td>
+                                                {{ producto.nombre }}
+                                            </td>
                                             <td class="fw-bold">
-                                                    $299
+                                                    ${{ producto.precio }}
                                             </td>
                                             <td class="flex align-items-start gap-4">
                                                 <button
@@ -56,7 +64,7 @@
                                                 >
                                                     -
                                                 </button>
-                                                    1
+                                                    {{ producto.cantidad }}
                                                 <button
                                                     type="button"
                                                     class="btn btn-dark"
